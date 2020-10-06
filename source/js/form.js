4e0ab8f22bf52208305d27576f677605c6830ed8
Dmitriy Solovev <dmptts@gmail.com>
2020-10-06 18:05:15 +0300

    Правки валидации формы

--- source/js/form.js
+++ source/js/form.js
@@ -4,23 +4,25 @@
   var nameInput = document.querySelector('#callback-name-field');
   var phoneInput = document.querySelector('#callback-phone-field');
 
-  var validateName = function () {
-    nameInput.setCustomValidity('');
+  if (nameInput && phoneInput) {
+    nameInput.addEventListener('change', function (evt) {
+      evt.preventDefault();
+      nameInput.setCustomValidity('');
 
-    if (!nameInput.checkValidity()) {
-      nameInput.setCustomValidity('Имя должно состоять из кириллических букв');
-    }
-  };
+      if (!nameInput.checkValidity()) {
+        nameInput.setCustomValidity('Имя должно состоять из кириллических букв');
+      }
+    });
 
-  var validatePhone = function () {
-    phoneInput.setCustomValidity('');
+    phoneInput.addEventListener('change', function (evt) {
+      evt.preventDefault();
+      phoneInput.setCustomValidity('');
 
-    if (!phoneInput.checkValidity()) {
-      phoneInput.setCustomValidity('Номер телефона должен состоять из 11 цифр, например 89998887766');
-    }
-  };
+      if (!phoneInput.checkValidity()) {
+        phoneInput.setCustomValidity('Номер телефона должен состоять из 11 цифр, например 89998887766');
+      }
+    });
+  }
 
-  nameInput.addEventListener('change', validateName);
-  phoneInput.addEventListener('change', validatePhone);
 
 })();
