ef4581a24101b70367bf10511bed093c908944d9
Dmitriy Solovev <dmptts@gmail.com>
2020-10-06 21:23:19 +0300

    Вносит различные мелкие исправления

--- build/js/main.js
+++ build/js/main.js
@@ -4,6 +4,8 @@
   var pageHeader = document.querySelector('.page-header');
   var burgerBtn = document.querySelector('.page-header__burger');
   var nav = document.querySelector('.page-header__nav');
+  var nameInput = document.querySelector('#callback-name-field');
+  var phoneInput = document.querySelector('#callback-phone-field');
 
   var initPage = function () {
     pageHeader.classList.remove('page-header--no-js');
@@ -33,5 +35,25 @@
     burgerBtn.addEventListener('click', onBurgerEnterPress);
 
     initPage();
+  }
+
+  if (nameInput && phoneInput) {
+    nameInput.addEventListener('change', function (evt) {
+      evt.preventDefault();
+      nameInput.setCustomValidity('');
+
+      if (!nameInput.checkValidity()) {
+        nameInput.setCustomValidity('Имя должно состоять из кириллических букв');
+      }
+    });
+
+    phoneInput.addEventListener('change', function (evt) {
+      evt.preventDefault();
+      phoneInput.setCustomValidity('');
+
+      if (!phoneInput.checkValidity()) {
+        phoneInput.setCustomValidity('Номер телефона должен состоять из 11 цифр, например 89998887766');
+      }
+    });
   }
 })();
